78081ef973b49d8dd24e300431c13162996c3410
sfernandez <sfdezlop@gmail.com>
2023-03-04 16:07:11 +0100

    Add rest of files

--- src/repository/log.file.repo.ts
+++ src/repository/log.file.repo.ts
@@ -1,12 +1,11 @@
 import fs from 'fs/promises';
-import { Repo } from './repo.interface';
 import { Log } from '../entities/log';
 import createDebug from 'debug';
 const debug = createDebug('RRSS:repo:log');
 
 const file = './data/data.json';
 
-export class LogFileRepo implements Repo<Log> {
+export class LogFileRepo {
   constructor() {
     debug('Instantiated at LogFileRepo');
   }
